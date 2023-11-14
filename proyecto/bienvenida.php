1e6d4aa7aeda5ccbb60c4754900506619925d628
Moises024 <126703271+Moises024@users.noreply.github.com>
2023-11-14 11:55:09 -0400

    Add files via upload

---
+++ proyecto/bienvenida.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning-Em</title>
+    <link rel="icon" type="image/png" href="img/logo.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="bienvenida.css">
+</head>
+<body>
+
+    <h1 id="titulo">Learning-Em</h4>
+    <img id="logo" src="img/logo.png" alt="Learning-Em">
+
+
+    <h4 class="text">La plataforma que te brinda el conocimiento.</h4>
+
+    <button><strong><a href="inicio.html">Explorar</a></strong></button>
+
+    <img class="imagen" src="img/imagen.jpg" alt="Chica">
+
+
+    <footer>
+        <div class="footer-content">
+            <ul class="redes">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+        </div>
+        <div class="footer-a">
+            <p>©Copyright all rights reserved.</p>
+        </div>
+    </footer>   
+        
+</body>
+</html>
