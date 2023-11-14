086d338148b912c27533953b2ac3edc6b77373bf
Moises024 <126703271+Moises024@users.noreply.github.com>
2023-11-14 12:16:57 -0400

    Nuevos php
    Contienen las siguientes partes: Contactos, Sobre Nosotros y la Bienvenida.

---
+++ sobrenosotros.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Sobre nosotros</title>
+    <link rel="icon" type="image/png" href="img/logo.png">
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="sobreNosotros.css">
+</head>
+<body>
+    <div class="barra">
+
+        <nav>
+            <a href="inicio.html">INICIO</a>
+            <a href="#">CURSOS</a>
+            <a href="contacto.html">CONTACTOS</a>
+            <a href="#">SOBRE NOSOTROS</a> 
+
+        </nav>
+
+        <h4 id="titulo">Learning-Em</h4>
+        <img id="logo" src="img/logo.png" alt="Learning-Em">
+
+    </div>
+
+    <img class="sn-1"  src="img/sn-1.jpg" alt="grupo">
+
+    <br></br>
+
+    
+
+    <p>¡Bienvenido a Learning-Em, la emocionante plataforma de educación en línea diseñada específicamente para inspirar y capacitar a personas
+        apasionadas por la programación! Nos enorgullece ofrecer una amplia variedad de cursos con un método de inscripción sencillo para que 
+        puedas comenzar tu viaje de aprendizaje sin complicaciones.</p>
+
+    <p>Nuestra plataforma se distingue por contar con una amplia gama de cursos cuidadosamente diseñados por profesionales de la industria 
+        con una gran experiencia en programación. Nos aseguramos de que nuestros cursos estén estructurados de manera lógica y progresiva, 
+        lo que te permite aprender desde los fundamentos de la programación hasta conceptos más avanzados.</p>
+        
+    <p>En Learning-Em, encontrarás una impresionante variedad de lenguajes de programación cubiertos. Puedes explorar cursos sobre lenguajes 
+        populares como Python, Java, C++, JavaScript, Ruby, PHP, entre muchos otros. Además, ofrecemos cursos especializados en diversas 
+        tecnologías y frameworks como Django, React, Angular, Node.js, y mucho más. De esta manera, puedes elegir cursos específicos según tus
+        intereses y necesidades.</p>
+
+        <img src="img/sn-2.jpg" align="right" width="210px" height="150px"  alt="chico">
+
+    <p>Nos enorgullece decir que en Learning-Em no solo nos enfocamos en la teoría, sino que también ponemos un fuerte énfasis en la
+        práctica. Nuestros cursos incluyen una combinación de videos instructivos, ejercicios prácticos y proyectos reales que te permitirán 
+        aplicar los conceptos aprendidos en situaciones de la vida real. Esto fortalecerá tus habilidades de resolución de problemas y 
+        fomentará tu creatividad en la programación.</p>
+        
+    <p>Nos aseguramos de ofrecer contenido de la más alta calidad. Todos nuestros cursos son creados y actualizados regularmente por 
+        expertos en la materia, lo que garantiza que siempre recibirás información precisa y actualizada. Nuestros videos instructivos son de
+        alta calidad, con explicaciones claras y ejemplos prácticos que te ayudarán a comprender los conceptos de manera efectiva. Además, 
+        nuestros ejercicios y proyectos prácticos están diseñados para desafiarte y ayudarte a consolidar tus conocimientos clave.</p>
+        
+    <p>En Learning-Em, estamos comprometidos con tu éxito y te acompañaremos en cada paso de tu viaje de aprendizaje. ¡Únete a nosotros y 
+        descubre todo lo que puedes lograr en el apasionante mundo de la programación!</p>
+    
+
+    <footer>
+        <div class="footer-content">
+            <ul class="redes">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+
+            </ul>
+        </div>
+        <div class="footer-a">
+            <p>©Copyright all rights reserved.</p>
+
+        </div>
+      
+      </footer>
+</body>
+</html>
