312ca6cb438d7d62000a1812fc10f759bbf7951c
Moises024 <126703271+Moises024@users.noreply.github.com>
2023-11-18 02:45:59 -0400

    PHP recientes

---
+++ card.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Bootstrap Example</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/card.css">
+  </head>
+  <body class="p-3 m-0 border-0 bd-example m-0 border-0">
+
+
+    
+    <div class="card" style="width: 16rem;"> <img src="assets/img/curso1.png" alt="curso1">
+      <div class="card-body">
+        <h6 class="card-title">Fundamentos de Desarrollo Web con HTML y CSS</h6>
+        <p class="card-text">Es una introducción completa al mundo del desarrollo web, centrándose en dos de los lenguajes más importantes y ampliamente utilizados: HTML (HyperText Markup Language) y CSS (Cascading Style Sheets).</p>
+        <a href="cursos.html" class="btn btn-primary">Ir al curso</a>
+      </div>
+    </div>
+    
+   
+  </body>
+</html>
