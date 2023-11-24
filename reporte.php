771deae4ffb3ef11a2a9120ecb3d5b846a66ac13
Moises024 <126703271+Moises024@users.noreply.github.com>
2023-11-24 02:15:35 -0400

    Reporte y certificado nuevos

---
+++ reporte.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reporte</title>
+    <link rel="stylesheet" href="css/reporte.css">
+</head>
+<body>
+
+    <img src="assets/img/logo.png" alt="logo">
+
+    <h1>Learning-Em</h1>
+    
+    <h3>Moisés Ricardo Zabala Bueno</h2>
+
+    <h5>24/11/2023</h5>
+        
+        
+    <h4>Asunto: Informe de Finalización de Curso en Línea</h3>
+    
+    <div>
+            
+        <p>Estimado/a Moisés Ricardo Zabala Bueno, nos complace informarle que ha completado satisfactoriamente el curso en línea titulado "Programación avanzada" ofrecido por Learning-EM. A través de este informe, nos gustaría felicitarle por su logro y compartir algunos detalles sobre su desempeño en el curso.</p>
+        
+        <p>Nos complace reconocer su esfuerzo y determinación para completar el curso con éxito. Esperamos que los conocimientos adquiridos sean beneficiosos para su desarrollo personal y profesional. Adjunto a este informe se encuentra su certificado de finalización del curso, que confirma su participación y logro.</p>
+        
+        <p>Una vez más, felicitaciones por su finalización exitosa del curso en línea "Programación avanzada". Agradecemos su participación y le deseamos mucho éxito en sus futuros esfuerzos de aprendizaje.</p>
+        
+        <p>Atentamente, coordinador Enrique Andrade<br>Learning-EM</p>
+
+    </div>
+
+</body>
+</html>
